8514f34f1cfc2c13d079a6836d971d6f7f6aeb23
Luciano Balmaceda <balmacedaluciano@gmail.com>
2019-05-01 12:59:12 -0300

    dynamically obtain the package version for telemetry

--- src/main/java/com/auth0/RequestProcessorFactory.java
+++ src/main/java/com/auth0/RequestProcessorFactory.java
@@ -2,6 +2,8 @@ package com.auth0;
 
 import com.auth0.client.auth.AuthAPI;
 import com.auth0.jwk.JwkProvider;
+import com.auth0.net.Telemetry;
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.lang3.Validate;
 
 import java.io.UnsupportedEncodingException;
@@ -15,6 +17,7 @@ class RequestProcessorFactory {
         Validate.notNull(responseType);
 
         AuthAPI client = new AuthAPI(domain, clientId, clientSecret);
+        setupTelemetry(client);
         return new RequestProcessor(client, responseType, null);
     }
 
@@ -25,6 +28,7 @@ class RequestProcessorFactory {
         Validate.notNull(responseType);
 
         AuthAPI client = new AuthAPI(domain, clientId, clientSecret);
+        setupTelemetry(client);
         TokenVerifier verifier = new TokenVerifier(clientSecret, clientId, domain);
         return new RequestProcessor(client, responseType, verifier);
     }
@@ -37,8 +41,21 @@ class RequestProcessorFactory {
         Validate.notNull(provider);
 
         AuthAPI client = new AuthAPI(domain, clientId, clientSecret);
+        setupTelemetry(client);
         TokenVerifier verifier = new TokenVerifier(provider, clientId, domain);
         return new RequestProcessor(client, responseType, verifier);
     }
 
+    @VisibleForTesting
+    void setupTelemetry(AuthAPI client) {
+        Telemetry telemetry = new Telemetry("auth0-java-mvc-common", obtainPackageVersion());
+        client.setTelemetry(telemetry);
+    }
+
+    @VisibleForTesting
+    String obtainPackageVersion() {
+        //Value if taken from jar's manifest file.
+        //Call will return null on dev environment (outside of a jar)
+        return RequestProcessorFactory.class.getPackage().getImplementationVersion();
+    }
 }
